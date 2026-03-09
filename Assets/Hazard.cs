408a379f1e034e348c390e5c4f088356ef88bd8f
Rosana Josepha <rosanaj1616@gmail.com>
2026-03-09 19:47:36 -0400

    added Hazard.cs script to spikes and minor changes
    Minor changes:
    - Reduced the "tunnel" to one block and repositioned/scaled to make it an "L" shape

---
+++ Assets/Hazard.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
