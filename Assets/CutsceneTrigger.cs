dcd259059439a555fe3129db4b7edbcdbc616949
Rosana Josepha <rosanaj1616@gmail.com>
2026-04-07 21:00:51 -0400

    added script to disable player movement during cutscene

--- Assets/CutsceneTrigger.cs
+++ Assets/CutsceneTrigger.cs
@@ -1,18 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CutsceneTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        // add button that will continue through dialogue until cutscene is done
+        // when dialogue finished, proceed to EndCutscene()
+        if (Input.GetKeyDown(KeyCode.Q)) // sample keycode test
+        {
+            EndCutscene();
+        }
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StartCutscene();
+        }
+    }
+
+    public void StartCutscene()
+    {
+        // add dialogue/flashback UI panel here
+
+        Time.timeScale = 0f;
+        Debug.Log("Cutscene started...");
+    }
+
+    public void EndCutscene()
+    {
+        // close dialogue/flashback UI panel here
+
+        Time.timeScale = 1f;
+        Debug.Log("Cutscene ended...");
+    }
+    
 }
