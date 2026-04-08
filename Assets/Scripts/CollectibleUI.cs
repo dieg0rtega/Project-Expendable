03d66398519f7275b5be0f36474c750cbc7dfeca
Fabianne Olmedo <folme002@fiu.edu>
2026-04-08 19:12:21 -0400

    Updated Dialogue Box Functionality

--- Assets/Scripts/CollectibleUI.cs
+++ Assets/Scripts/CollectibleUI.cs
@@ -8,11 +8,42 @@ public class CollectibleUI : MonoBehaviour
   public GameObject Collectpanel;
   public TextMeshProUGUI textBox;
 
+  private int currentPage = 1;
+
   public void Show(string message)
   {
     Collectpanel.SetActive(true);
     textBox.text = message;
+
+
+    currentPage = 1;
+    textBox.pageToDisplay = currentPage;
+
     Time.timeScale = 0f;
+  }
+
+  public void NextPage()
+  {
+    if ( currentPage < textBox.textInfo.pageCount)
+    {
+      currentPage++;
+      textBox.pageToDisplay = currentPage;
+    }
+    else
+    {
+      Hide();
+    }
+
+
+  }
+
+  public void PreviousPage()
+  {
+    if (currentPage > 1)
+    {
+      currentPage--;
+      textBox.pageToDisplay = currentPage;
+    }
   }
 
   public void Hide()
