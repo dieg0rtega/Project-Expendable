8f15bb0f1bba6db14d9671c2167b3c681d8318bf
Diego <160441988+dieg0rtega@users.noreply.github.com>
2026-04-14 02:58:46 -0400

    Added Cave Backdrop to Level 2

--- Assets/2_Scripts/Environment/ParallaxLayer.cs.cs
+++ Assets/2_Scripts/Environment/ParallaxLayer.cs.cs
@@ -1,9 +1,21 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 [ExecuteInEditMode]
 public class ParallaxLayer : MonoBehaviour
 {
     public float parallaxFactor;
+    public bool followY = false;
+
+    private void Update()
+    {
+        if (followY)
+        {
+            Vector3 newPos = transform.localPosition;
+            newPos.y = GameObject.Find("Main Camera").transform.position.y;
+            transform.localPosition = newPos;
+        }
+    }
 
     public void Move(float delta)
     {
